d1a4c45c479145877e43a932005132d8ca89146c
Dani <dani_rl94@hotmail.com>
2016-02-20 04:48:47 +0100

    map

--- WiiServer/core/src/com/noxer/games/wii/Partida.java
+++ WiiServer/core/src/com/noxer/games/wii/Partida.java
@@ -3,11 +3,16 @@ package com.noxer.games.wii;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Partida implements Screen{
 	private PerspectiveCamera cam, cam2;
@@ -15,32 +20,46 @@ public class Partida implements Screen{
     static final int WORLD_HEIGHT = 100;
     private Sprite background;
     private Batch batch;
+    private TiledMap tiledMap;
+    TiledMapRenderer tiledMapRenderer;
+    private OrthographicCamera camera;
+    
 	
 	public Partida(){
 		float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,w,h);
+        camera.update();
+        batch = new SpriteBatch();
+        tiledMap = new TmxMapLoader().load("mapa.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
         
-		cam = new PerspectiveCamera(40, 15, 15 * (h / w));
-		cam2 = new PerspectiveCamera(40, 15, 15 * (h / w));
+        System.out.println(tiledMap.getProperties().get("width",Integer.class));
+        
+		cam = new PerspectiveCamera(30, 15, 15 * (h / w));
+		cam2 = new PerspectiveCamera(30, 15, 15 * (h / w));
 		
 		//cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(0f, 0f, -20f);
-        cam.lookAt(0,0,0);
-        cam.near = 1f;
+        cam.position.set(15f, 15f, -20f);
+        cam.lookAt(15,15,0);
+        cam.near = 0.1f;
         cam.far = 300f;
         cam.rotate(-50, 1, 0, 0);
         cam.update();
         
-        cam2.position.set(0f, 0f, -20f);
-        cam2.lookAt(0,0,0);
-        cam2.near = 1f;
+        cam2.position.set(15f, 15f, -20f);
+        cam2.lookAt(15,15,0);
+        cam2.near = 0.1f;
         cam2.far = 300f;
         cam2.rotate(-50, 1, 0, 0);
         cam2.update();
         
+        
+        
         background = new Sprite(new Texture("fondo2.png"));
         background.setSize(WORLD_WIDTH, WORLD_HEIGHT);
-        batch = new SpriteBatch();
+        background.setPosition(0, 0);
 	}
 
 	@Override
@@ -58,21 +77,26 @@ public class Partida implements Screen{
 	    bottomStage.act(delta);    */     
 		cam.update();
         batch.setProjectionMatrix(cam.combined);
+        Gdx.gl.glViewport(0,Gdx.graphics.getHeight()/2,
+        		Gdx.graphics.getWidth(),Gdx.graphics.getHeight()/2);      
         batch.begin();
 	    /*Upper Half*/          
 	    //set the openGl viewport to half the screenheight and starting y from the     middle of the screen
-	    Gdx.gl.glViewport(0,Gdx.graphics.getHeight()/2,
-	    		Gdx.graphics.getWidth(),Gdx.graphics.getHeight()/2);      
-	    background.draw(batch);
+	    //background.draw(batch);
+	    //tiledMapRenderer.setView(camera);
+        tiledMapRenderer.setView(cam.combined, 0, Gdx.graphics.getHeight()/2,
+        		Gdx.graphics.getWidth(),Gdx.graphics.getHeight()/2);
+        tiledMapRenderer.render();
 	    batch.end();
+	    
 
 	    cam2.update();
         batch.setProjectionMatrix(cam2.combined);
+        Gdx.gl.glViewport(0,0,
+        		Gdx.graphics.getWidth(),Gdx.graphics.getHeight()/2);    
 	    batch.begin();
 	    /*bottom Half*/     
 	    //set the openGl viewport to half the screenheight and starting y from the     bottom of the screen
-	    Gdx.gl.glViewport(0,0,
-	    		Gdx.graphics.getWidth(),Gdx.graphics.getHeight()/2);    
 	    background.draw(batch);
 	    batch.end();
 
@@ -80,9 +104,9 @@ public class Partida implements Screen{
 
 	@Override
 	public void resize(int width, int height) {
-		cam.viewportWidth = 30f;
+		cam.viewportWidth = 20f;
         cam.viewportHeight = 30f * height/width;
-        cam2.viewportWidth = 30f;
+        cam2.viewportWidth = 20f;
         cam2.viewportHeight = 30f * height/width;
         cam.update();
         cam2.update();
