9ab83552d2bcfecb48edc892940a172d03a81ce0
Dani <dani_rl94@hotmail.com>
2016-02-20 01:34:58 +0100

    partida

---
+++ WiiServer/core/src/com/noxer/games/wii/Partida.java
@@ -0,0 +1,53 @@
+package com.noxer.games.wii;
+
+import com.badlogic.gdx.Screen;
+
+public class Partida implements Screen{
+	
+	public Partida(){
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
